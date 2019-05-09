cc1f6943892deeaa8747f2575f52509e36dd311a
AislynnEdmiston <davidandaislynn@gmail.com>
2019-05-09 19:45:10 -0400

    finished dropdown, started on tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,18 +3,18 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     // this.element;
-    
+    this.element = element;
     // Get the custom data attribute on the Link
     // this.data;
-    
+    this.data = this.element.dataset.tab;
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
-    
+    this.item = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
-    
+    this.tabitem = new TabItem(this.item);
     // Add a click event listener on this instance, calling the select method on click
-
+    this.element.addEventListener('click', () => this.select());
   };
 
   select() {
